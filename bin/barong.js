23e6f134ebb8cc9c246f78c943422bddce6d327b
Enang Yusup and Hazmi Abdun Nazir <pair+enang+hazmi@kmkonline.co.id>
2015-10-30 11:18:33 +0700

    [ref #106625414] basic CLI feature

--- bin/barong.js
+++ bin/barong.js
@@ -1,5 +1,12 @@
 #! /usr/bin/env node
 
-"use strict"
+"use strict";
 
+var BarongLib = require('../lib/barongLib.js');
+
+var command = process.argv[2];
+
+if(command === "capture"){
+  BarongLib.capture();
+}
 console.log("Wraaarrrhh!!");
