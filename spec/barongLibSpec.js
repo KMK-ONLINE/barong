bc6de4d85d2dbbf9b9ac717d3bb2010f45fa5a65
Enang Yusup and Hazmi Abdun Nazir <pair+enang+hazmi@kmkonline.co.id>
2015-11-02 17:18:28 +0700

    refactor capture lib

--- spec/barongLibSpec.js
+++ spec/barongLibSpec.js
@@ -6,12 +6,36 @@ var sizeOf = require('image-size');
 var BarongLib = require('../lib/barongLib.js');
 var Util = require('../lib/util.js');
 
+var CONFIG_TMP_FILE = '.barong-config.json';
+
 describe("BarongLib", function(){
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 
+  describe("saveConfigTmp", function(){
+    var tmpPath = path.join(__dirname, CONFIG_TMP_FILE);
+
+    beforeAll(function(){
+      if(fs.existsSync(tmpPath)){
+        fs.unlink(tmpPath);
+      }
+    });
+
+    afterAll(function(){
+      fs.unlink(tmpPath);
+    });
+
+    it("save the config json file to tmp file", function(){
+      BarongLib.saveConfigTmp(__dirname, {"base": "/path/base.json"});
+
+      var expected = fs.existsSync(tmpPath);
+      expect(expected).toBe(true);
+    });
+  });
+
   describe("capture", function(){
-    var dirname = path.join(__dirname, '..', 'bitmaps_test');
-    var outputFile = Util.generateFilename('Test Base', 'Test Page');
+    var cwd = path.join(__dirname, '..');
+    var dirname = path.join(cwd, 'bitmaps_test');
+    var outputFile = Util.generateFilename('Test Page', 'Test Capture');
 
     var configJSON = {
       "label": "Test Base",
@@ -21,6 +45,13 @@ describe("BarongLib", function(){
       },
       "capture_target": "bitmaps_test",
       "scenarios": [
+      ]
+    };
+
+    var files = {
+      "base": "/path/to/base.json",
+      "tests": [
+        "/path/to/test-folder/page.json"
       ]
     };
 
@@ -34,7 +65,7 @@ describe("BarongLib", function(){
     });
 
     afterAll(function(){
-      var targetFile = configJSON.scenarios[0].output_file;
+      var targetFile = configJSON.scenarios[0].captures[0].output_file;
       var dirname = path.dirname(targetFile);
       fs.unlink(targetFile);
       fs.rmdir(dirname);
@@ -43,17 +74,26 @@ describe("BarongLib", function(){
     describe("with css selector", function(){
       beforeAll(function(done){
         configJSON.scenarios[0] = {
-          "label": "Test Page",
-          "url": "http://www.liputan6.dev:8000",
-          "selector": ".navbar--top__logo",
-          "output_file": path.join(dirname, outputFile + '.png')
+          "label"    : "Test Page",
+          "url"      : "http://www.liputan6.dev:8000",
+          "captures" : [
+            {
+              "label": "Test Capture",
+              "selector"    : ".navbar--top__logo",
+              "output_file" : path.join(dirname, outputFile + '.png')
+            }
+          ]
         };
         spyOn(Util, "readConfig").and.returnValue(configJSON);
-        BarongLib.capture(done);
+        BarongLib.capture(cwd, '', done);
+      });
+
+      it("called Util.readConfig with the correct params", function(){
+        expect(Util.readConfig).toHaveBeenCalledWith(cwd, files);
       });
 
       it("can capture the selector", function(){
-        var targetFile = configJSON.scenarios[0].output_file;
+        var targetFile = configJSON.scenarios[0].captures[0].output_file;
         var result = fs.existsSync(targetFile);
         expect(result).toBe(true);
       });
@@ -65,22 +105,28 @@ describe("BarongLib", function(){
         configJSON.scenarios[0] = {
           "label": "Test Page",
           "url": "http://www.liputan6.dev:8000",
-          "selector": {
-            "top": 0,
-            "left": 0,
-            "width": 200,
-            "height": 400
-          },
-          "output_file": path.join(dirname, outputFile + '.png')
+          "captures": [
+            {
+              "label": "Test Capture",
+              "selector": {
+                "top": 0,
+                "left": 0,
+                "width": 200,
+                "height": 400
+              },
+              "output_file": path.join(dirname, outputFile + '.png')
+            }
+          ]
         };
-        targetFile = configJSON.scenarios[0].output_file;
+        targetFile = configJSON.scenarios[0].captures[0].output_file;
         spyOn(Util, "readConfig").and.returnValue(configJSON);
-        BarongLib.capture(done);
+        BarongLib.capture('', '', done);
       });
 
       it("can capture a region in a page", function(){
         var result = fs.existsSync(targetFile);
         expect(result).toBe(true);
+        expect(Util.readConfig).toHaveBeenCalledWith('', files);
       });
 
       it("capture the correct size", function(){
